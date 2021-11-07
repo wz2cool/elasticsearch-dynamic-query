b895e5db4714d5a6dce5e2ca084ef517720de00a
wz2cool <wz2cool@live.cn>
2021-11-07 09:13:57 +0800

    fix build issue

--- src/main/java/io/github/wz2cool/exception/PropertyNotFoundInternalException.java
+++ src/main/java/io/github/wz2cool/exception/PropertyNotFoundInternalException.java
@@ -2,7 +2,6 @@ package io.github.wz2cool.exception;
 
 /**
  * @author Frank
- * @date 2021/11/06
  **/
 public class PropertyNotFoundInternalException extends RuntimeException {
     public PropertyNotFoundInternalException(String message) {
