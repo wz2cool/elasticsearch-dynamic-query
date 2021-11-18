fbafa299618d93a87e4fed06fd4961dbd349eca9
wz2cool <wz2cool@live.cn>
2021-11-18 20:56:23 +0800

    fix test

--- src/main/java/com/github/wz2cool/elasticsearch/query/IElasticsearchQuery.java
+++ src/main/java/com/github/wz2cool/elasticsearch/query/IElasticsearchQuery.java
@@ -1,11 +1,13 @@
 package com.github.wz2cool.elasticsearch.query;
 
 
+import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.search.sort.SortBuilder;
 import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
 import org.springframework.util.CollectionUtils;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 /**
@@ -19,10 +21,17 @@ public interface IElasticsearchQuery {
         StringBuilder stringBuilder = new StringBuilder();
         final List<String> sortStrings = nativeSearchQuery.getElasticsearchSorts()
                 .stream().map(SortBuilder::toString).collect(Collectors.toList());
+        QueryBuilder queryBuilder;
+        if (Objects.nonNull(nativeSearchQuery.getQuery())) {
+            queryBuilder = nativeSearchQuery.getQuery();
+        } else {
+            queryBuilder = nativeSearchQuery.getFilter();
+        }
+
         final String sortItems = String.join(",", sortStrings);
         stringBuilder.append("{")
                 .append("\"query\":")
-                .append(nativeSearchQuery.getQuery().toString()).append(",");
+                .append(queryBuilder.toString()).append(",");
         if (!CollectionUtils.isEmpty(nativeSearchQuery.getElasticsearchSorts())) {
             stringBuilder.append("\"sort\":[")
                     .append(sortItems).append("],");
