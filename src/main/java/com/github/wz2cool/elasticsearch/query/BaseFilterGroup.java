159588269e137c7f35376efb1d45d0eead100f68
wz2cool <wz2cool@live.cn>
2021-11-18 21:35:31 +0800

    fix test

--- src/main/java/com/github/wz2cool/elasticsearch/query/BaseFilterGroup.java
+++ src/main/java/com/github/wz2cool/elasticsearch/query/BaseFilterGroup.java
@@ -11,24 +11,29 @@ public abstract class BaseFilterGroup<T, S extends BaseFilterGroup<T, S>> extend
     /// region group
 
     public S and(UnaryOperator<FilterGroup<T>> groupConsumer) {
-        return and(true, null, groupConsumer);
+        return and(true, groupConsumer);
     }
 
     public S and(boolean enable, UnaryOperator<FilterGroup<T>> groupConsumer) {
-        return and(enable, null, groupConsumer);
-    }
-
-    public S and(FilterMode filterMode, UnaryOperator<FilterGroup<T>> groupConsumer) {
-        return and(true, filterMode, groupConsumer);
-    }
-
-    public S and(boolean enable, FilterMode filterMode, UnaryOperator<FilterGroup<T>> groupConsumer) {
         if (!enable) {
             return (S) this;
         }
         FilterGroup<T> filterGroup = new FilterGroup<>();
         final QueryBuilder queryBuilder = groupConsumer.apply(filterGroup).getFilterQuery();
-        return andInternal(filterMode, queryBuilder);
+        return andInternal(FilterMode.MUST, queryBuilder);
+    }
+
+    public S andNot(UnaryOperator<FilterGroup<T>> groupConsumer) {
+        return andNot(true, groupConsumer);
+    }
+
+    public S andNot(boolean enable, UnaryOperator<FilterGroup<T>> groupConsumer) {
+        if (!enable) {
+            return (S) this;
+        }
+        FilterGroup<T> filterGroup = new FilterGroup<>();
+        final QueryBuilder queryBuilder = groupConsumer.apply(filterGroup).getFilterQuery();
+        return andInternal(FilterMode.MUST_NOT, queryBuilder);
     }
 
     public S or(UnaryOperator<FilterGroup<T>> groupConsumer) {
@@ -44,23 +49,27 @@ public abstract class BaseFilterGroup<T, S extends BaseFilterGroup<T, S>> extend
     }
 
     public S and(FilterGroup<T> filterGroup) {
-        return and(true, null, filterGroup);
+        return and(true, filterGroup);
     }
 
     public S and(boolean enable, FilterGroup<T> filterGroup) {
-        return and(enable, null, filterGroup);
-    }
-
-    public S and(FilterMode filterMode, FilterGroup<T> filterGroup) {
-        return and(true, filterMode, filterGroup);
-    }
-
-    public S and(boolean enable, FilterMode filterMode, FilterGroup<T> filterGroup) {
         if (!enable) {
             return (S) this;
         }
         final QueryBuilder queryBuilder = filterGroup.getFilterQuery();
-        return andInternal(filterMode, queryBuilder);
+        return andInternal(FilterMode.MUST, queryBuilder);
+    }
+
+    public S andNot(FilterGroup<T> filterGroup) {
+        return andNot(true, filterGroup);
+    }
+
+    public S andNot(boolean enable, FilterGroup<T> filterGroup) {
+        if (!enable) {
+            return (S) this;
+        }
+        final QueryBuilder queryBuilder = filterGroup.getFilterQuery();
+        return andInternal(FilterMode.MUST_NOT, queryBuilder);
     }
 
     public S or(FilterGroup<T> filterGroup) {
@@ -79,22 +88,25 @@ public abstract class BaseFilterGroup<T, S extends BaseFilterGroup<T, S>> extend
     /// region custom
 
     public S and(QueryBuilder queryBuilder) {
-        return and(true, null, queryBuilder);
+        return and(true, queryBuilder);
     }
 
     public S and(boolean enable, QueryBuilder queryBuilder) {
-        return and(enable, null, queryBuilder);
-    }
-
-    public S and(FilterMode filterMode, QueryBuilder queryBuilder) {
-        return and(true, filterMode, queryBuilder);
-    }
-
-    public S and(boolean enable, FilterMode filterMode, QueryBuilder queryBuilder) {
         if (!enable) {
             return (S) this;
         }
-        return andInternal(filterMode, queryBuilder);
+        return andInternal(FilterMode.MUST, queryBuilder);
+    }
+
+    public S andNot(QueryBuilder queryBuilder) {
+        return andNot(true, queryBuilder);
+    }
+
+    public S andNot(boolean enable, QueryBuilder queryBuilder) {
+        if (!enable) {
+            return (S) this;
+        }
+        return andInternal(FilterMode.MUST_NOT, queryBuilder);
     }
 
     public S or(QueryBuilder queryBuilder) {
