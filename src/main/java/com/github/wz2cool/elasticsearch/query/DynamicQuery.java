8901f2af4292e8fadbfd5e6c9101b6faf3f78bec
deng <dengmeiluan@163.com>
2021-12-23 11:28:33 +0800

    add route

--- src/main/java/com/github/wz2cool/elasticsearch/query/DynamicQuery.java
+++ src/main/java/com/github/wz2cool/elasticsearch/query/DynamicQuery.java
@@ -9,6 +9,7 @@ import com.github.wz2cool.elasticsearch.model.ColumnInfo;
 import com.github.wz2cool.elasticsearch.model.PropertyInfo;
 import com.github.wz2cool.elasticsearch.model.QueryMode;
 import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
 import org.elasticsearch.search.sort.FieldSortBuilder;
 import org.elasticsearch.search.sort.ScoreSortBuilder;
@@ -32,6 +33,8 @@ public class DynamicQuery<T> extends BaseFilterGroup<T, DynamicQuery<T>> impleme
     private final HighlightBuilder highlightBuilder = new HighlightBuilder();
     private final List<SortBuilder> sortBuilders = new ArrayList<>();
     private final QueryMode queryMode;
+    private final String route;
+
     private String[] selectedColumns = new String[]{};
     private String[] ignoredColumns = new String[]{};
 
@@ -51,18 +54,26 @@ public class DynamicQuery<T> extends BaseFilterGroup<T, DynamicQuery<T>> impleme
         this.ignoredColumns = ignoredColumns;
     }
 
-
-    private DynamicQuery(Class<T> clazz, QueryMode queryMode) {
+    public DynamicQuery(Class<T> clazz, QueryMode queryMode, String route) {
         this.clazz = clazz;
         this.queryMode = queryMode;
+        this.route = route;
     }
 
     public static <T> DynamicQuery<T> createQuery(Class<T> clazz) {
-        return new DynamicQuery<>(clazz, QueryMode.QUERY);
+        return new DynamicQuery<>(clazz, QueryMode.QUERY, null);
     }
 
     public static <T> DynamicQuery<T> createQuery(Class<T> clazz, QueryMode queryMode) {
-        return new DynamicQuery<>(clazz, queryMode);
+        return new DynamicQuery<>(clazz, queryMode, null);
+    }
+
+    public static <T> DynamicQuery<T> createQuery(Class<T> clazz, String route) {
+        return new DynamicQuery<>(clazz, QueryMode.QUERY, route);
+    }
+
+    public static <T> DynamicQuery<T> createQuery(Class<T> clazz, QueryMode queryMode, String route) {
+        return new DynamicQuery<>(clazz, queryMode, route);
     }
 
     public DynamicQuery<T> scoreMapping(BiConsumer<T, Float> setScorePropertyFunc) {
@@ -148,6 +159,9 @@ public class DynamicQuery<T> extends BaseFilterGroup<T, DynamicQuery<T>> impleme
     @Override
     public NativeSearchQuery buildNativeSearch() {
         NativeSearchQueryBuilder esQuery = new NativeSearchQueryBuilder();
+        if (StringUtils.isNotBlank(route)) {
+            esQuery.withRoute(route);
+        }
         if (getQueryMode() == QueryMode.QUERY) {
             esQuery.withQuery(getFilterQuery());
         } else {
